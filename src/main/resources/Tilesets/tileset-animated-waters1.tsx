300c0718b29b3fdf48f40389929a54aaa0dd13c7
Chris Luby <clubycoder@gmail.com>
2019-04-15 20:39:23 -0600

    Work on game assets

---
+++ src/main/resources/Tilesets/tileset-animated-waters1.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Tileset Animated Water1" tilewidth="64" tileheight="64" tilecount="40" columns="10">
+ <image source="../Textures/tiles/tileset-animated-waters1.png" width="640" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="9" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ src/main/resources/Tilesets/tileset-forest-decor1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Tileset Forest Decorations1" tilewidth="64" tileheight="128" tilecount="32" columns="8">
+ <image source="../Textures/tiles/tileset-forest-decor1.png" width="512" height="512"/>
+</tileset>
